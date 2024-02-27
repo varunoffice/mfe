5f9a3c7a31ae6ec7ae7b7b9b28f9afc4d076ac22
varun <varun.office89@gmail.com>
2024-02-27 21:39:27 +0530

    container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module:{
         rules:[
@@ -20,5 +22,10 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
 }
